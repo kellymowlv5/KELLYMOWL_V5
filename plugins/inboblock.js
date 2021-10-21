373133aa7684e4663103aa025cfc144f003f23ea
ғͥᴀͭsᷤɪᷟʟͤ <84661141+fasilvkn@users.noreply.github.com>
2021-10-21 16:54:13 +0530

    Update inboblock.js

--- plugins/inboblock.js
+++ plugins/inboblock.js
@@ -7,7 +7,7 @@ const Config = require('../config')
 
 // Inbox Block System
 // This Plugins By Hisham Muhammed 
-const INBO = "pm not allowed bro🥲* https://chat.whatsapp.com/HIGByrNtUoxA6JvJJscW0T"
+const INBO = "pm not allowed bro🥲 https://chat.whatsapp.com/HIGByrNtUoxA6JvJJscW0T"
  if (Config.INBO == 'true') {
 Hisham.addCommand({on: 'text', fromMe: false, delownsewcmd: false, onlyPm: true }, (async (message, match) => {
         let regexb1ichu = new RegExp('.')
